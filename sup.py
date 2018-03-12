76edf68be06d65dae20bd0a95de4bea18e1458c2
Tuomas Starck <dev@starck.fi>
2018-03-12 16:05:35 +0200

    Serve static files also
    Also:
    * Catch upload hook permissions error
    * Configure max file size limit in megabytes

--- sup.py
+++ sup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# TODO: better headers, filename mangle support
+# TODO: better headers, filename mangle support, add magic support
 
 import sys
 
@@ -10,20 +10,25 @@ from uuid import uuid4
 from datetime import datetime
 from subprocess import run, CalledProcessError
 
-from flask import Blueprint, Flask, render_template, request
 from werkzeug.utils import secure_filename
+from flask import (Blueprint, Flask,
+    render_template, request, send_from_directory)
 
 
 DEF_EXTENSIONS = 'asc bin txt log json yml yaml pdf gif jpg jpeg png gz xz zip'
 DEF_INTRO = 'Please start the file upload by first selecting the file.'
 
+PREFIX = environ.get('URL_PREFIX', '/sup')
 blueprint = Blueprint('sup', __name__, template_folder='tmpl')
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
-def index():
+@blueprint.route('/<filename>', methods=['GET'])
+def index(filename=None):
     if request.method == 'POST':
         return upload_request(request)
+    elif filename:
+        return send_from_directory('static', filename)
     else:
         return render_template('upload.html',
             title=app.config['title'],
@@ -66,6 +71,9 @@ def upload_request(req):
         except FileNotFoundError as fnfe:
             say('{}', fnfe)
             return 'Upload hook not found', 500
+        except PermissionError as pe:
+            say('{}', pe)
+            return 'Upload hook permission failed', 500
     return 'OK'
 
 
@@ -80,7 +88,7 @@ def get_config():
         'allow_filenames': bool(environ.get('ALLOW_FILENAMES', False)),
         'allow_extensions':
             environ.get('ALLOW_EXTENSIONS', DEF_EXTENSIONS).split(),
-        'MAX_CONTENT_LENGTH': int(environ.get('MAX_SIZE', '1')) }
+        'MAX_CONTENT_LENGTH': 1024*1024*int(environ.get('MAX_FILE_SIZE', '1'))}
 
 
 def main():
@@ -89,16 +97,17 @@ def main():
     if not path.isdir(app.config['upload_dir']):
         print('upload directory does not exist', file=sys.stderr)
         sys.exit(1)
-    say('Allowed extensions: {}', ' '.join(app.config['allow_extensions']))
-    say('Allow user defined filenames: {}', app.config['allow_filenames'])
-    say('Upload directory: {}', app.config['upload_dir'])
+    say('URL prefix: {}', PREFIX)
     say('Upload hook: `{}`', app.config['upload_hook'])
-    say('URL prefix: {}', environ.get('URL_PREFIX', '/sup'))
+    say('Upload directory: {}', app.config['upload_dir'])
+    say('File size limit: {} B', app.config['MAX_CONTENT_LENGTH'])
+    say('Allow user defined filenames: {}', app.config['allow_filenames'])
+    say('Allowed extensions: {}', ' '.join(app.config['allow_extensions']))
     app.run(host=app.config['addr'], port=app.config['port'], debug=debug)
 
 
 app = Flask(__name__)
-app.register_blueprint(blueprint, url_prefix=environ.get('URL_PREFIX', '/sup'))
+app.register_blueprint(blueprint, url_prefix=PREFIX)
 
 if __name__ == '__main__':
     main()
